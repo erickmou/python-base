731a2e2be9f102492f13e08becad91378ff9f505
erickmou <erickmou@gmail.com>
2023-09-30 08:14:22 -0300

    Meu primeiro Arquivo git

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Primeiro damos permissão de execução ao script na linha 
+#de comando do terminal.
+#chmod +x hello.py
+
+print("Hello, World!")
+print('erick'.upper())
+# Especificando o interpretador na linha de comando
+#python3 hello.py
+
+# Usando o interpretador especificado na 
+#'#!/usr/bin/env python3'
+#./hello.py
