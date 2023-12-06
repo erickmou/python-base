9d1a1ec0601f3ad162d284c13773ec2fdddac6b7
erickmou <erickmou@hotmail.com>
2023-12-06 07:07:39 -0300

    pacotes idioma do sistema

--- hello.py
+++ hello.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python3
 # Especificando o interpretador na linha de comando
+"""
+Hello World Multi Liguas.
 
-print("Hello, World!")
+Dependendo da ligua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada exemplo:
+
+    export LANG=pt_BR
+
+Execução:
+
+    pythons3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Erick MOura"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG","en_US")[:5]
+
+msg = "Hello, World!" 
+
+if current_language == "pt_BR":
+   msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+   msg = "Ciao, Mondo"
+
+print(msg)
 
 # Dar  permissão de execução ao script na linha de comando do terminal.
 # "chmod +x hello.py"
 
 # ./hello.py = python3 hello.py
-# ou executando comando  "mv hello.py hello" ou "mv hello.py hello.exe"
+# ou executando comando  "mv hello.py hello" ou "mv hello.py hello.e"
 # ./hello = python3 hello.py
